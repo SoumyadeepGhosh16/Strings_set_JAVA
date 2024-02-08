d485633a022a302ababef30e73410b5fa12e941a
Soumyadeep Ghosh <139796191+SoumyadeepGhosh16@users.noreply.github.com>
2024-02-09 00:56:00 +0530

    Add files via upload

---
+++ String_4.java
@@ -0,0 +1,16 @@
+public class String_4 {
+    public static void main(String[] args) {
+        
+        String s1="Soumyadeep";
+        String s2="Ghosh";
+
+        int strlen=s1.length();
+        int strlen2=s2.length();
+
+        if(strlen==strlen2){
+            System.out.println("length of both strings are same");
+        }else{
+            System.out.println("length of the two strings are equal");
+        }
+    }
+}
